5ece4c36e12a5af572bc35eb254d877754532600
Do Nguyen Van <do.nguyen@ntq-solution.com.vn>
2019-07-28 23:54:52 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,24 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Currency msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Currency from './components/Currency.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Currency
   }
 }
 </script>
 
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Montserrat', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/Currency.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="hello">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-10 ml-auto mr-auto">
+          <div class="row">
+            <div class="col-lg-3">
+              <label>From</label>
+              <input type="text" class="form-control" placeholder="From" />
+            </div>
+            <div class="col-lg-1">
+              <i class="fa fa-exchange" aria-hidden="true"></i>
+            </div>
+            <div class="col-lg-3">
+              <label>To</label>
+              <input type="text" class="form-control" placeholder="To" />
+            </div>
+            <div class="col-lg-3">
+              <label>Amount</label>
+              <input type="number" v-model="amount" class="form-control"/>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      amount: 1
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.fa-exchange {
+  position: relative;
+  top: 65%;
+  transform: translateY(-50%);
+}
+</style>
